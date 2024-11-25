3db8345c8ce0cee5bfe4ccbbbe6c3e07d4ab8e3f
PratikPanda <pratic.panda@gmail.com>
2024-11-26 00:02:40 +0530

    Python Day 1 - Started Python by Angela Yu - Basic Python Codes, Variables, tring Concatination, len() etc

---
+++ Python/Day 1/main.py
@@ -0,0 +1,39 @@
+# 6. Printing "Hello World" in Python
+print('Hello World')
+
+# Test
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# New Line Character
+print("\n")
+
+# New Line with single Print
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of "
+      "Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+print("\n")
+
+# 7. String Concatenation
+print("Hello" + " " + "Pratik")
+
+# Debugging Practice
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+# 8. Input
+#input("What is your Name?")
+
+# print("Hello " + input("What is your Name? ") + "!")
+
+# 9. Python Variables
+name = input("What is your Name? ")
+print("Hello " + name)
+print("Length of " + name + " is: " + str(len(name)))
+
