7e576148389c52673a53be29be90e2742cd51b7a
PratikPanda <pratic.panda@gmail.com>
2025-01-12 23:32:35 +0530

    Array Methods - map, find, filter, some, every, includes, indexOf, findIndex

--- React/JSConcepts/index.js
+++ React/JSConcepts/index.js
@@ -34,7 +34,7 @@ const arr_1 = [1, 2, 3, 4, 5]
 const [arr1, arr2, arr3, arr4] = arr_1
 console.log(arr1, arr2, arr3, arr4)
 
-// ================================ [ Default Parameters, spread and ret operators ]
+// ================================ [ Default Parameters, spread and rest operators ]
 // Default parameters
 function mulOf2nums(num1 = 1, num2 = 2){    // If parameter not passed, it will take this default value
     return num1 * num2;
@@ -67,3 +67,92 @@ function getInfo(a, b, ...c){        // ...c will keep all other arguments, it w
 
 
 console.log(getInfo(1, 2, 3, 4, 5));
+
+
+// ===================================== [ Array Methods - map, find, filter, some, every, includes, indexOf, findIndex ]
+const personsArray = [
+    {
+        name: "PRATIK",
+        age: 28,
+        country: "India"
+    },
+    {
+        name: "John Doe",
+        age: 45,
+        country: "Russia"
+    },
+    {
+        name: "Alester",
+        age: 30,
+        country: "UK"
+    },
+    {
+        name: "sWARAJ",
+        age: 30,
+        country: "India"
+    },
+]
+
+
+// map -> go through all the elements
+console.log("============================= Map");
+let getAllNames = personsArray.map((singlePerson, index) => {
+    console.log(singlePerson)
+    console.log(singlePerson.name)
+    return `${singlePerson.name} age is: ${singlePerson.age} and is from country ${singlePerson.country}`
+});
+
+console.log(getAllNames);
+
+// find -> Finds the first match
+console.log("============================= Find");
+let getFirstPersonFromIndia = personsArray.find((singlePerson, index) => {
+        // if(singlePerson.country === "India"){
+        //     return `${singlePerson.name} is from India`;
+        // }
+
+    return singlePerson.country === "India"
+});
+
+console.log(getFirstPersonFromIndia);
+
+// filter -> return all the matches based on condition
+console.log("============================= Filter");
+let getPersonFromIndia = personsArray.filter((singlePerson, index) => {
+    return singlePerson.country === "India"
+});
+
+console.log(getPersonFromIndia);
+
+// Some -> Return true if there is atleast one match satisfying the condition
+console.log("============================= Some");
+let checkAllPersonsAdult = personsArray.some((singlePerson, index) => {
+    return singlePerson.age >= 20
+});
+
+console.log(checkAllPersonsAdult);
+
+// Every -> Return true if all matches satisfying the condition
+console.log("============================= Every");
+let checkEveryPerson = personsArray.every((singlePerson, index) => {
+    return singlePerson.age > 40
+});
+
+console.log(checkEveryPerson);
+
+// Includes -> Checks if the element in present in the Array or not
+console.log("============================= Includes");
+let fruits = ["Apple", "Banana", "Orange", "Guava"];
+console.log(fruits.includes("Apple"));
+
+// indexOf -> Returns index of the element if it exists in the Array
+console.log("============================= indexOf");
+console.log(fruits.indexOf("Orange"));
+
+// findIndex ->
+console.log("============================= findIndex");
+let getIndexOfRussia = personsArray.findIndex((singlePerson, index) => {
+    return singlePerson.country === "Russia"
+});
+
+console.log(getIndexOfRussia);
